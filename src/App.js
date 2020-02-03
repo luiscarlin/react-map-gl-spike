50d5a26c1985a62a262bb698bd3fc25a996f665e
Luis Carlin <luiscarlin@gmail.com>
2020-02-03 12:52:37 -0500

    get it runing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import MapGL from "react-map-gl";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const MAPBOX_TOKEN = "your token here";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewport: {
+        latitude: 37.8,
+        longitude: -122.4,
+        zoom: 14,
+        bearing: 0,
+        pitch: 0
+      }
+    };
+  }
+
+  render() {
+    return (
+      <MapGL
+        {...this.state.viewport}
+        width="100vw"
+        height="100vh"
+        mapStyle="mapbox://styles/mapbox/dark-v9"
+        onViewportChange={viewport => this.setState({ viewport })}
+        mapboxApiAccessToken={MAPBOX_TOKEN}
+      />
+    );
+  }
 }
 
 export default App;
